273c7297b9476a6839b6e0386d488dc571b14a29
Steven Jimenez <stevenmjimenez@gmail.com>
2026-06-14 00:17:56 -0400

    feat: add Parsed interface for representing parse results

---
+++ toolchain/compiler/src/parser/parse.ts
@@ -0,0 +1,11 @@
+/**
+ * Result of a successful parse operation.
+ *
+ * Parsers are implemented as pure functions that consume tokens starting at a
+ * given position and return both the parsed AST node and the index of the next
+ * unconsumed token.
+ */
+export interface Parsed<T> {
+    readonly node: T;
+    readonly next: number;
+}
